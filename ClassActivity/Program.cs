59c31449f7b4b24dd031bdd301445644a96dd29d
jacob <jacob@DELINE_LAPTOP>
2024-03-20 08:09:21 +0200

    Add project files.

---
+++ ClassActivity/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Declare arrays
+        string[] studentName = new string[4];
+        string[] studentGender = new string[4];
+        string[] studentDOB = new string[4];
+
+        //display student info
+        Console.WriteLine("Student Information:");
+        for (int i = 0; i < studentName.Length; i++)
+        {
+            Console.WriteLine($"Student {i + 1}: ");
+
+            //prompts user to enter their name
+            Console.Write($"Name: ");
+            studentName[i] = Console.ReadLine();
+
+            //prompts user to enter their gendder
+            Console.Write($"Gender: ");
+            studentGender[i] = Console.ReadLine();
+
+            //promps user to enter their date of birth
+            Console.Write($"DOB: ");
+            studentDOB[i] = Console.ReadLine();
+
+        }
+
+
+    }
+}
